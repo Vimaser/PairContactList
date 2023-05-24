7fa2c23895209c783628288ccb9148bc1da1a02d
Vimaser <thfeske@gmail.com>
2023-05-24 18:58:57 -0500

    mappingData

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Main = () => {
+  const [contacts, setContacts] = useState(dummyContacts);
+ 
+  {
+    contacts.map(contact => {
+      return contact.name;
+    })
+  }
+
   return (
     <div id="main">
       <div id="navbar">
         <div>Contact List</div>
       </div>
-      <div id="container">{/* your components here */}</div>
+      <div id="container">{
+      
+      }</div>
     </div>
   );
 };
 
 export default Main;
+import dummyContacts from '../dummyData'
